2eaf2da0773b0bb12f13e52789c3655b370cfb67
Antara Ganapathy <antaraganapathy@gmail.com>
2024-09-11 22:17:43 +0530

    nav

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,14 +7,14 @@ const Home = () => {
   return (
     <section className='relative w-full h-screen mx-auto'>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
-        <div className='flex flex-col justify-center items-center mt-5'>
+        {/* <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
           <div className='w-1 sm:h-80 h-40 violet-gradient'/>
-        </div>
+        </div> */}
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi! My name is <span className='text-[#915eff]'>Antara</span></h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}> Lorem ipsum dolor sit amet, <br className='sm:block hidden' /> consectetur adipiscing elit </p>
+          <h1 className={`${styles.heroSubText} mt-2 text-white-100`}> Lorem ipsum dolor sit amet, <br className='sm:block hidden' /> consectetur adipiscing elit </h1>
         </div>
       </div>
 
