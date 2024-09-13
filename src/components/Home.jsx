b456526562a289750e8786682c99476a469a66ae
Antara Ganapathy <antaraganapathy@gmail.com>
2024-09-13 11:42:34 +0530

    scroll

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,7 +22,9 @@ const Home = () => {
       
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
-          <div className={`${styles.scroll1}`}></div>
+          <div className={`${styles.scroll1}`}>
+            <div className={`${styles.scroll2}`}></div>
+          </div>
           {/* <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
               animate={{
